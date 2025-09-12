dfe00a0f9b7d9eb53675b42a4aaa56b04b2da64f
Muhammad Bashir <bashirkarimi5@gmail.com>
2025-09-12 08:53:01 +0200

    fixes types

--- apps/blog/src/components/blog.tsx
+++ apps/blog/src/components/blog.tsx
@@ -1,25 +1,25 @@
 import { PostCard } from "./post-card";
-import { POSTS_QUERYResult } from "@/sanity/types";
+import { BlogList } from "@/sanity/types";
 
 interface BlogProps {
-  data?: POSTS_QUERYResult;
+  data?: BlogList;
 }
 
 const Blog = ({ data }: BlogProps) => {
   return (
     <section
-      className="w-full  px-4 py-24 mx-auto sm:px-6 lg:px-8"
+      className="w-full px-4 py-24 mx-auto sm:px-6 lg:px-8"
       aria-labelledby="blog-title"
     >
       <h2
         id="blog-title"
-        className="text-3xl font-bold text-center text-gray-900 sm:text-4xl"
+        className="text-3xl font-bold text-gray-900 sm:text-4xl"
       >
-        From the blog
+        {data?.title ?? "From the blog"}
       </h2>
-      {data?.length && (
+      {data?.posts && (
         <ul className="grid grid-cols-1 md:grid-cols-2 gap-6">
-          {data.map((post) => (
+          {(data.posts).map((post: any) => (
             <li key={post._id}>
               <PostCard post={post} />
             </li>
--- apps/blog/src/components/section-renderer.tsx
+++ apps/blog/src/components/section-renderer.tsx
@@ -1,9 +1,10 @@
+import { BlogList } from "../sanity/types";
 import { Blog } from "./blog";
 
-const SectionRenderer = ({ section }: { section: any }) => {
+const SectionRenderer = ({ section }: { section: BlogList }) => {
   switch (section._type) {
     case "blogList":
-      return <Blog data={section.posts} />;
+      return <Blog data={section} />;
     default:
       return null;
   }
