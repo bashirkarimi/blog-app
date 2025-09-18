1dbd195865c6fa75d7a3611b61a5b27815634eeb
Muhammad Bashir <bashirkarimi5@gmail.com>
2025-09-18 12:38:35 +0200

    removes PostList and Posts

--- apps/studio/schemaTypes/objects/sections/index.ts
+++ apps/studio/schemaTypes/objects/sections/index.ts
@@ -3,7 +3,6 @@ import {accordionType} from '../../modules/accordion'
 import {imageTeaserType} from '../../modules/image-teaser'
 import {richTextType} from '../../modules/rich-text'
 import {teaserListType} from '../../modules/teaser-list'
-import { postsType } from '../../modules/posts'
 
 export const sectionTypes = [
   blogListType,
@@ -11,5 +10,4 @@ export const sectionTypes = [
   imageTeaserType,
   richTextType,
   teaserListType,
-  postsType,
 ]
