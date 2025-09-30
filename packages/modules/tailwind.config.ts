88e5d701d34c02d9993d86c10bdc8020dbcf0c21
Muhammad Bashir <bashirkarimi5@gmail.com>
2025-09-30 09:51:32 +0200

    fix: add tailwind config to scan classes in the pakcages

--- packages/modules/tailwind.config.ts
+++ packages/modules/tailwind.config.ts
@@ -1,7 +1,8 @@
-import type { Config } from "tailwindcss"
-const config: Pick<Config, "presets" | "content"> = {
-  content: ["./src/**/*.tsx"],
-  presets: [require("@repo/tailwind-config/postcss")],
-}
-
-export default config
+// Tailwind config for local development of @repo/modules.
+// Primary generation for production should occur in consuming apps, but this
+// allows running build:styles for preview. Keep globs narrow here.
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+};
