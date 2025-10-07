217c4d4f9b4bed5900f572c0a257c7a5444e1b3b
Muhammad Bashir <bashirkarimi5@gmail.com>
2025-10-07 09:21:15 +0200

    chore: style background of text

--- packages/modules/src/modules/hero/hero.tsx
+++ packages/modules/src/modules/hero/hero.tsx
@@ -20,7 +20,7 @@ const Hero: ComponentType<HeroProps> = ({ data }) => (
     )}
     {/* dark overlay */}
     <div className="absolute inset-0 bg-gray-900/60" aria-hidden="true" />
-    <div className="relative w-full md:w-1/2 lg:w-1/3 p-8 mt-auto text-white z-10">
+    <div className="relative w-full md:w-1/2 lg:w-1/3 p-8 mt-auto text-white z-10 backdrop-blur-xs bg-gray-900/30">
       <h2 className="text-4xl font-bold">{data.title}</h2>
       {data.text && (
         <div className="mt-4 prose prose-invert max-w-none">
