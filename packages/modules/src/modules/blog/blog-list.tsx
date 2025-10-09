ae5e5e722608255741e502b9c56210d367a00023
Muhammad Bashir <bashirkarimi5@gmail.com>
2025-10-09 08:46:33 +0200

    chore: remove section

--- packages/modules/src/modules/blog/blog-list.tsx
+++ packages/modules/src/modules/blog/blog-list.tsx
@@ -12,15 +12,9 @@ const BlogList = ({ data }: BlogListProps) => {
   if (initialPosts.length === 0) return null;
 
   return (
-    <section className="w-full mx-auto" aria-labelledby="blog-title">
-      <h2
-        id="blog-title"
-        className="text-3xl font-bold text-gray-900 sm:text-4xl"
-      >
-        {data?.title ?? "From the blog"}
-      </h2>
+    <div>
       <BlogItem initialPosts={initialPosts} limit={limit} />
-    </section>
+    </div>
   );
 };
 
