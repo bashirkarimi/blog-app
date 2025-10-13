019dfeee80dca18db1847e503bcc92ab6f87d9a9
Muhammad Bashir <bashirkarimi5@gmail.com>
2025-10-13 16:09:51 +0200

    chore: remove link type from Sections

--- apps/studio/schemaTypes/objects/index.ts
+++ apps/studio/schemaTypes/objects/index.ts
@@ -1,7 +1,9 @@
 import { heroTypes } from './heros';
 import { sectionTypes } from './sections';
+import {linkType} from '../modules/link';
 
 export const sharedObjectTypes = [
   ...heroTypes,
   ...sectionTypes,
+  linkType
 ];
